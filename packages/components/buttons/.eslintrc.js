3b32057be54a669e0a37038b4cc84b15128fa9a8
agolez13 <antonius.golez@cyara.com>
2025-01-08 12:14:57 +1100

    add eslint config

--- packages/components/buttons/.eslintrc.js
+++ packages/components/buttons/.eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
-	extends: ['react-app', 'react-app/jest', 'prettier'],
-	rules: {
-		'react/self-closing-comp': ['error'],
-		'@typescript-eslint/no-explicit-any': ['error'],
-	},
-}
+  extends: ["react-app", "react-app/jest", "prettier"],
+  rules: {
+    "react/self-closing-comp": ["error"],
+    "@typescript-eslint/no-explicit-any": ["error"],
+  },
+};
---
+++ packages/components/buttons/eslint.config.js
@@ -0,0 +1,9 @@
+export default [
+  {
+    files: ["**/*.js", "**/*.ts", "**/*.tsx"],
+    rules: {
+      "no-unused-vars": "warn",
+      "react/react-in-jsx-scope": "off", // Example rule
+    },
+  },
+];
